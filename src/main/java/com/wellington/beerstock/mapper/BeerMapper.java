d92d4bbdc56f353ee51b949e6a187985b3455582
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-12-01 12:25:33 -0300

    API Beer Stock - DIO

---
+++ src/main/java/com/wellington/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.wellington.beerstock.mapper;
+
+import com.wellington.beerstock.dto.BeerDTO;
+import com.wellington.beerstock.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
